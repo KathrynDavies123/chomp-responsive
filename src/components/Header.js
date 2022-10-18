a58908828e91cc87914d39cb81e620018651c1cd
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2022-10-18 08:51:41 +0200

    header mobile

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Chomp!</h1>
+      <h2>It’s like Lieferando for dogs!</h2>
+    </header>
+  );
+};
+
+export default Header;
