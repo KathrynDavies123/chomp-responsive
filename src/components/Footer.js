1bec383fa92a4e94f6a53df14d89a32874719de9
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2022-10-19 17:46:21 +0200

    finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,26 @@
 import "./Footer.css";
+import facebook from "../img/icons8-facebook.png";
+import instagram from "../img/icons8-instagram.png";
+import twitter from "../img/icons8-twitter.png";
+
+const Footer = () => {
+    return ( <footer>
+        <div className="signupform">
+            <p>Sign up to be notified when we launch in your area!</p>
+            <form>
+                <input type="email" placeholder="Email Address"/>
+                <input type="submit" value="Sign Up"/>
+            </form>
+        </div>
+        <div className="socialcopyright">
+            <div className="socialmediaicons">
+                <img src={facebook} alt="facebook icon"/>
+                <img src={instagram} alt="instagram icon"/>
+                <img src={twitter} alt="twitter icon"/>
+            </div>
+            <p className="copyright">© 2020 Chomp Inc.</p>
+        </div>
+    </footer> );
+}
+ 
+export default Footer
